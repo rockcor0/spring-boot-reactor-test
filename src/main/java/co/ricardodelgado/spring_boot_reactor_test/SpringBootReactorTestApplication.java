60cf000e6bd171e0d1efda1cff1d900c8daa4b69
Ricardo Delgado <ridel007@gmail.com>
2024-10-24 19:55:47 -0500

    Refactoring the code

--- src/main/java/co/ricardodelgado/spring_boot_reactor_test/SpringBootReactorTestApplication.java
+++ src/main/java/co/ricardodelgado/spring_boot_reactor_test/SpringBootReactorTestApplication.java
@@ -7,44 +7,83 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @SpringBootApplication
 public class SpringBootReactorTestApplication implements CommandLineRunner {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SpringBootReactorTestApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SpringBootReactorTestApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactorTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactorTestApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
+		sampleCreateObservable();
+		sampleIterable();
+		sampleFlatMap();
+    }
 
-		//Create an observable
+    public void sampleCreateObservable(String... args) {
 
-		//From a List
-		Flux<String> fromList = Flux.fromIterable(List.of(args));
+        //Create an observable
 
-		//From an array
-		Flux<String> fromString = Flux.fromArray(args);
+        //From a List
+        Flux<String> fromList = Flux.fromIterable(List.of(args));
 
-		//From some data
-		Flux<User> names = Flux.just("Richie", "Lilly", "Isa", "Gandalf", "Arya", "Zelda")
-				.map(name -> new User(name, null))
-				.doOnNext(user -> {
-					if(user == null){
-						throw new RuntimeException("The name can't be empty");
-					}
-					System.out.println(user);
-				});
+        //From an array
+        Flux<String> fromString = Flux.fromArray(args);
 
-		names.subscribe(
-				user -> LOG.info(user.name()),
-				error -> LOG.error(error.getMessage()),
-				() -> LOG.info("Finished with success!")
-		);
+        //From some data
+        Flux<String> names = Flux.just("Richie", "Lilly", "Isa", "Gandalf", "Arya", "Zelda");
 
-	}
+        //Subscriber
+        names.subscribe(
+                LOG::info,
+                error -> LOG.error(error.getMessage()),
+                () -> LOG.info("Finished with success!")
+        );
+    }
+
+    public void sampleIterable() {
+
+        //Create an observable
+
+        //From some data
+        Flux<User> names = Flux.just("Richie", "Lilly", "Isa", "Gandalf", "Arya", "Zelda")
+                .map(name -> new User(name, null))
+                .doOnNext(user -> {
+                    if (user == null) {
+                        throw new RuntimeException("The name can't be empty");
+                    }
+                    System.out.println(user);
+                });
+
+        names.subscribe(
+                user -> LOG.info(user.name()),
+                error -> LOG.error(error.getMessage()),
+                () -> LOG.info("Finished with success!")
+        );
+    }
+
+    public void sampleFlatMap() {
+
+        //Create an observable
+
+        //From some data
+        Flux<User> userFlux = Flux.just("Richie", "Lilly", "Isa", "Gandalf", "Arya", "Zelda")
+                .map(name -> new User(name, null))
+                .flatMap(user -> user.name().equalsIgnoreCase("gandalf") ?
+                        Mono.just(user) :
+                        Mono.empty());
+
+        userFlux.subscribe(
+                user -> LOG.info(user.name()),
+                error -> LOG.error(error.getMessage()),
+                () -> LOG.info("Finished with success!")
+        );
+    }
 }
